bb945726dea438fb45f4f26dd08536e8ed3b7983
RFilho01 <robertocorreiafilho0100@outlook.com>
2022-03-12 22:18:00 -0300

    automating databaseaccess

--- features/cadastro.feature
+++ features/cadastro.feature
@@ -5,35 +5,35 @@ Funcionalidade: Cadastro
   Quero fazer o meu cadastro no Rocklov
   Para que eu possa disponibilizá-los para locação
 
-  @temp
+  @cadastro
   Cenario: Fazer cadastro 
     
     Dado que acesso a página de cadastro
     Quando submeto o meu cadastro completo
     Então sou redirecionado para o Dashboard
 
-  @sem_nome
+  @tentativa_cadastro
   Cenario: Submeter cadastro sem o nome
 
     Dado que acesso a página de cadastro
     Quando submeto o meu cadastro sem o nome
     Então vejo a mensagem de alerta: Oops. Informe seu nome completo!
 
-
+  @tentativa_cadastro
   Cenario: Submeter cadastro sem o email
 
     Dado que acesso a página de cadastro
     Quando submeto o meu cadastro sem o email
     Então vejo a mensagem de alerta: Oops. Informe um email válido!
 
-
+  @tentativa_cadastro
   Cenario: Submeter cadastro com email inválido
 
     Dado que acesso a página de cadastro
     Quando submeto o meu cadastro com email inválido
     Então vejo a mensagem de alerta: Oops. Informe um email válido!
 
-
+  @tentativa_cadastro
   Cenario: Submeter cadasto sem a senha
     
     Dado que acesso a página de cadastro
--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,17 +1,20 @@
+
+
 Dado('que acesso a página de cadastro') do
   visit "rocklov-web:3000/signup"
 end
 
 Quando('submeto o meu cadastro completo') do
+  MongoDB.new.remove_user("roberto@gmail.com")
+
   find("#fullName").set "Roberto Filho"
-  find("#email").set Faker::Internet.free_email
+  find("#email").set "roberto@gmail.com"
   find("#password").set "pwd123"
   click_button("Cadastrar")
 end
 
 Então('sou redirecionado para o Dashboard') do
   expect(page).to have_css ".dashboard"
-  sleep 10
 end
 
 
@@ -24,4 +27,35 @@ end
 Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
   alert = find(".alert-dark")
   expect(alert.text).to eql "Oops. Informe seu nome completo!"
+end
+
+
+Quando('submeto o meu cadastro sem o email') do
+  find("#fullName").set "Roberto Filho"
+  find("#password").set "pwd123"
+  click_button("Cadastrar")
+end
+
+Quando('submeto o meu cadastro com email inválido') do
+  find("#fullName").set "Roberto Filho"
+  find("#email").set "roberto*filho.com"
+  find("#password").set "pwd123"
+  click_button("Cadastrar")
+end
+
+Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe um email válido!"
+end
+
+
+Quando('submeto o meu cadastro sem a senha') do
+  find("#fullName").set "Roberto Filho"
+  find("#email").set Faker::Internet.free_email
+  click_button("Cadastrar")
+end
+
+Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+  alert = find(".alert-dark")
+  expect(alert.text).to eql "Oops. Informe sua senha secreta!"
 end
